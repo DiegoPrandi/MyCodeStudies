a700d190982358e1f2c8de404b3e11d25ce3ba63
diego prandi <diego.ps2@puccampinas.edu.br>
2026-03-12 22:01:15 -0300

    estudos

--- Projects/1 - LoginSystem/main.py
+++ Projects/1 - LoginSystem/main.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import os
+import getpass
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,3 +13,67 @@ conexao = mysql.connector.connect(
 )
 
 cursor  = conexao.cursor()
+
+def createAccount():
+    os.system('cls')
+    print('''   ___     _                              _        
+  / __\ __(_) __ _ _ __    ___ ___  _ __ | |_ __ _ 
+ / / | '__| |/ _` | '__|  / __/ _ \| '_ \| __/ _` |
+/ /__| |  | | (_| | |    | (_| (_) | | | | || (_| |
+\____/_|  |_|\__,_|_|     \___\___/|_| |_|\__\__,_|
+                                                   ''')
+    
+    nome = input('Nome: ')
+    email = input('Email: ')
+    senha = getpass.getpass('Senha: ')
+    
+    sql = 'INSERT INTO usuarios (nome, email, senha) VALUES(%s, %s, %s)'
+    values  = nome, email, senha
+    
+    try:
+        cursor.execute(sql, values)
+        conexao.commit()
+        os.system('cls')
+        print('Usuario criado com sucesso')
+        print('Pressione enter pra continuar...')
+        input('')
+        
+    except mysql.connector.Error as erro:
+        print('Erro:', erro)
+        input('')
+
+
+def main():
+    while True:
+        os.system('cls')
+        print('''                                                                                    
+    ,--.                 ,--.             ,---.                 ,--.                    
+    |  |    ,---.  ,---. `--',--,--,     '   .-',--. ,--.,---.,-'  '-. ,---. ,--,--,--. 
+    |  |   | .-. || .-. |,--.|      \    `.  `-. \  '  /(  .-''-.  .-'| .-. :|        | 
+    |  '--.' '-' '' '-' '|  ||  ||  |    .-'    | \   ' .-'  `) |  |  \   --.|  |  |  | 
+    `-----' `---' .`-  / `--'`--''--'    `-----'.-'  /  `----'  `--'   `----'`--`--`--' 
+                `---'                         `---'                                   ''')
+        print('''
+            1 - Criar conta
+            2 - Login
+            3 - Listar usuários
+            4 - Deletar usuário
+            0 - Sair
+            ''')
+        
+        op = int(input('Digite oq deseja fazer: '))
+        
+        match op:
+            case 1:
+                createAccount()
+            case 2:
+                login()
+            case 3:
+                listUsers()
+            case 4:
+                deleteUser()
+            case 5:
+                print('Até mais...')
+                break        
+
+main()
